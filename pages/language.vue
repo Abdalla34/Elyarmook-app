39e0d82493939eb68a955e490941bf54d19f5031
Abdalla34 <166306580+Abdalla34@users.noreply.github.com>
2025-06-15 17:54:28 +0300

    langPage

--- pages/language.vue
+++ pages/language.vue
@@ -1,5 +1,31 @@
 <template>
-    <div>
-        
+  <div>
+    <ProfileDetails />
+    <div class="profile-information">
+      <div class="container">
+        <div class="row">
+          <div class="col-8 col-padding">
+            <h1 class="margin-bottom-24px text-capitalize title-pages">
+              language
+            </h1>
+
+            <div class="lang margin-bottom-24px box-pages">
+              <div class="lang-box ">
+                <div class="lang-detalis">
+                  <div class=""></div>
+                  <div class="">
+                    <p></p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
+
+<script setup>
+
+</script>
