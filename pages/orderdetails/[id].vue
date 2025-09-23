832562c1423d62de326b58825e1fd640ccea15ca
Abdalla34 <166306580+Abdalla34@users.noreply.github.com>
2025-09-23 16:30:11 +0300

    add missing ordersteps.css

--- pages/orderdetails/[id].vue
+++ pages/orderdetails/[id].vue
@@ -459,7 +459,7 @@ function toFalse() {
 </script>
 
 <style scoped>
-@import "@/assets/css/ordersteps.css";
+@import "~/assets/css/ordersteps.css";
 .popup-sure-cancel {
   height: 12% !important;
   text-align: center;
