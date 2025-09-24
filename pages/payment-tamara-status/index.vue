ce358e180ae6ae5f3e7997fd592e0e178fd813b3
Abdalla34 <166306580+Abdalla34@users.noreply.github.com>
2025-09-24 15:47:59 +0300

    laoding in checking ordre payment done

--- pages/payment-tamara-status/index.vue
+++ pages/payment-tamara-status/index.vue
@@ -2,16 +2,21 @@
   <div class="text-green-600 text-3xl font-bold mb-4 text-center fs-3">
     جاري التحقق من حالة الدفع...
   </div>
+  <div class="load">
+    <loading-spinner :is-loading-otp="loading" />
+  </div>
 </template>
 
 <script setup>
 let isChecking = false;
 let route = useRoute();
 let router = useRouter();
+const loading = ref(false);
 
 onMounted(async () => {
   if (isChecking) return;
   isChecking = true;
+  loading.value = true;
 
   const rawId = route.query.id;
   if (!rawId) {
@@ -29,6 +34,8 @@ onMounted(async () => {
   } catch (err) {
     console.error(err);
     router.replace("/payment-tamara-status/failed");
+  } finally {
+    loading.value = false;
   }
 });
 </script>
